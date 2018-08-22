d5c5a96e19e30b76633268160fec94614b234d9e
Yao.Zeng <Yao.Zeng@windows10.microdone.cn>
2018-08-22 18:01:51 +0800

    实体修改

---
+++ charself/src/main/java/com/yzeng/charroom/entity/UserInfo.java
@@ -0,0 +1,18 @@
+package com.yzeng.charroom.entity;
+
+/**
+ * 用户信息资料
+ * @author yzengchn@163.com
+ * 2018-08-22 17:24:29
+ * @version 1.0.0
+ */
+public class UserInfo {
+	//用户ID
+	private Integer userId;
+	//用户名
+	private String userName;
+	//昵称
+	private String nickName;
+	//
+	
+}
--- charself/src/main/java/com/yzeng/charroom/entity/UserRelation.java
+++ charself/src/main/java/com/yzeng/charroom/entity/UserRelation.java
@@ -6,10 +6,49 @@ package com.yzeng.charroom.entity;
  * @version  [版本号, 2018年8月21日]
  */
 public class UserRelation {
-	//用户
+	//用户A
 	private Integer userIdA;
-	//用户
+	//用户B
 	private Integer userIdB;
-	//用户之间的关系状态 1：好友  2：
+	//用户之间的关系状态 1：好友  0:黑名单
 	private int relationStatus;
+	//添加时间
+	private String relationTime;
+	//用户A给用户B的备注名
+	private String memoName;
+	
+	public String getMemoName() {
+		return memoName;
+	}
+	public void setMemoName(String memoName) {
+		this.memoName = memoName;
+	}
+	public Integer getUserIdA() {
+		return userIdA;
+	}
+	public void setUserIdA(Integer userIdA) {
+		this.userIdA = userIdA;
+	}
+	public Integer getUserIdB() {
+		return userIdB;
+	}
+	public void setUserIdB(Integer userIdB) {
+		this.userIdB = userIdB;
+	}
+	public int getRelationStatus() {
+		return relationStatus;
+	}
+	public void setRelationStatus(int relationStatus) {
+		this.relationStatus = relationStatus;
+	}
+	public String getRelationTime() {
+		return relationTime;
+	}
+	public void setRelationTime(String relationTime) {
+		this.relationTime = relationTime;
+	}
+	public UserRelation() {
+		super();
+	}
+	
 }
